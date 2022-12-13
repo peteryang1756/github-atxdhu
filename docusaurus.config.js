37271588487b3475983372c04584da61a54aea09
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-13 20:34:43 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,14 +67,7 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} 雙龍體育`,
     },
   },
-plugins: ["@chatwoot/docusaurus-plugin"],
-  themeConfig: {
-    chatwoot: {
-      websiteToken: "Your website inbox token",
-      baseURL: "https://app.chatwoot.com",  // optional
-      enableInDevelopment: false,  // optional
-    }
-  }, 
+
 
   presets: [
     [
